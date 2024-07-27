608a86c460794bd44e530f19802c41b144fdf289
Samson <samitesfaye726@gmail.com>
2024-07-27 23:33:18 +0300

    Combine the name of their city and pet and show the their band name.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 #1. Create a greeting for my program
 #2. Ask the user for the city that they grew up in.
 #3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show the their band name.
