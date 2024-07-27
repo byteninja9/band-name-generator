27a035b89eceaf4088bedec2151275f75d387a78
Samson <samitesfaye726@gmail.com>
2024-07-27 23:58:41 +0300

    Completed Version

--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 #3. Ask the user for the name of a pet.
 #4. Combine the name of their city and pet and show the their band name.
 
-print("""
+from colorama import Fore, init as colorama_init
+
+
+colorama_init(autoreset=True)
+
+print(f"""{Fore.CYAN}
       
 \    /_ | _ _ ._ _  _   | _  |_) _.._  _| |\ | _.._ _  _
  \/\/(/_|(_(_)| | |(/_  |(_) |_)(_|| |(_| | \|(_|| | |(/_
@@ -11,3 +16,8 @@ print("""
                     \n\t\t  Created By: Byte Ninja                                                                                                                                                                                                                                                                                          
                                                                                                                                                                                                                                                                                              
       """)
+
+city = input("What's name of the city you grew up in?\n")
+pets = input("What's your pet's name?\n")
+
+print(f"Your band name could be {city} {pets}")
