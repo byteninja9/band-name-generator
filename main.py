3827a170607c174c7322074b87f1347872b0070e
Samson <samitesfaye726@gmail.com>
2024-07-27 23:50:09 +0300

    Create Banner For Greeting

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 #2. Ask the user for the city that they grew up in.
 #3. Ask the user for the name of a pet.
 #4. Combine the name of their city and pet and show the their band name.
+
+print("""
+      
+\    /_ | _ _ ._ _  _   | _  |_) _.._  _| |\ | _.._ _  _
+ \/\/(/_|(_(_)| | |(/_  |(_) |_)(_|| |(_| | \|(_|| | |(/_
+                    \n\t\t  Generator (V 0.1)
+                    \n\t\t  Created By: Byte Ninja                                                                                                                                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                             
+      """)
